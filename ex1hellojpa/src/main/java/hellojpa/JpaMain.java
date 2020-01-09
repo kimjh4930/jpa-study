48fd2e3422df74cd3ac1b877fd32fa47c938376e
Kim, Junha <kimjh4930@gmail.com>
2020-01-09 16:35:59 +0900

    ch.01 완료.

---
+++ ex1hellojpa/src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,34 @@
+package hellojpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.List;
+
+public class JpaMain {
+
+    public static void main(String args[]) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+
+        tx.begin();
+
+        try{
+            List<Member> findList = em.createQuery("select m from Member as m").getResultList();
+
+            for(Member m : findList){
+                System.out.println(m.getId() + ", " + m.getName());
+            }
+
+            tx.commit();
+        }catch (Exception e){
+            tx.rollback();
+        }finally{
+            em.close();
+        }
+
+        emf.close();
+    }
+}
