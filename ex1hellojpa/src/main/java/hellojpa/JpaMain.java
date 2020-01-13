3949bd3ac5bafa7b932f6b002a4b162215ef02d6
Kim, Junha <kimjh4930@gmail.com>
2020-01-13 22:30:15 +0900

    객체와 테이블 매핑, 데이터베이스 스키마 자동생성.

--- ex1hellojpa/src/main/java/hellojpa/JpaMain.java
+++ ex1hellojpa/src/main/java/hellojpa/JpaMain.java
@@ -15,17 +15,6 @@ public class JpaMain {
             //영속
             Member member = em.find(Member.class, 211L);
             member.setName("AAAAAAAA");
-
-            /**
-             * 준영속상태. JPA에서 관리하지 않음.
-             * commit 을 하더라도 DB에 반영되지 않음.
-             *
-             * 준영속 상태로 만드는 방법.
-             *  1. em.detach(Object);
-             *  2. em.clear()
-             *      - 다시 조회하면 (em.find()) 다시 영속성 컨텍스트에 올라간다.
-             *  3. em.close()
-             */
             em.detach(member);
 
             System.out.println("====================");
--- ex1hellojpa/src/main/java/hellojpa/Member.java
+++ ex1hellojpa/src/main/java/hellojpa/Member.java
@@ -1,13 +1,40 @@
 package hellojpa;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
+/**
+ * JPA가 관리하는 객체임을 의미함.
+ *  - name : 매핑할 테이블 이름.
+ *  아래와 같이 Query가 생성됨.
+ *    select
+ *         member0_.id as id1_0_0_,
+ *         member0_.name as name2_0_0_
+ *     from
+ *         MBR member0_ //Table 이름이 바뀐것을 확인 할 수 있음.
+ *     where
+ *         member0_.id=?
+ *
+ *  데이터베이스 스키아 자동생성
+ *      - DDL을 애플리케이션 실행 시점에 자동으로 생성함.
+ *      - 테이블중심 -> 객체중심
+ *      - 데이터베이스 방언을 활용해서 데이터베이스에 맞는 적절한 DDL을 생성함.
+ *          - 이 DDL 은 개발장비에서만 사용 할 것.
+ *          - 실무에서는 Table을 따로 생성하고 Mapping만 할 것.
+ *          - 필요할 경우에는 적절히 다듬어서 사용 할 것.
+ */
 @Entity
 public class Member {
 
     @Id
     private Long id;
+
+    /**
+     * JPA 실행에 영향을 주지 않음. DDL생성에만 영향을 준다.
+     * JPA 실행 로직에는 영향을 주지 않음.
+     */
+    @Column(unique = true, length = 10)
     private String name;
 
     protected Member (){}
