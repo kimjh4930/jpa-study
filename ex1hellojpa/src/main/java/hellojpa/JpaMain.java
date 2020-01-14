9eaf876ec5ff1860177956f4879de16bb6f1329f
Kim, Junha <kimjh4930@gmail.com>
2020-01-15 00:13:38 +0900

    연관관계 매핑 기초

--- ex1hellojpa/src/main/java/hellojpa/JpaMain.java
+++ ex1hellojpa/src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,16 @@ import javax.persistence.*;
 
 public class JpaMain {
 
+    /**
+     *  단방향 연관관계
+     *      - 객체와 테이블 연관관계의 차이를 이해
+     *      - 객체의 참조와 테이블의 외래 키를 매핑
+     *      - 용어 이해
+     *        - 방향(Direction) : 단방향, 양방향
+     *        - 다중성(Multiplicity) : 다대일(N:1), 일대다(1:N), 일대일(1:1), 다대다 (N:M)
+     *        - 연관관계 주인(Owner) : 객체 양방향 연관관계는 관리 주인이 필요.
+     */
+
     public static void main(String args[]) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
         EntityManager em = emf.createEntityManager();
@@ -12,25 +22,23 @@ public class JpaMain {
         tx.begin();
 
         try{
-            Member member1 = new Member();
-            member1.setUsername("A");
 
-            Member member2 = new Member();
-            member2.setUsername("B");
+            Team team = new Team();
+            team.setName("teamA");
+            em.persist(team);
 
-            Member member3 = new Member();
-            member3.setUsername("C");
+            Member member = new Member();
+            member.setName("member1");
+            member.setTeam(team);
+            em.persist(member);
 
+            em.flush();
+            em.clear();
 
-            System.out.println("=====================");
-            em.persist(member1);
-            em.persist(member2);
-            em.persist(member3);
+            Member findMember = em.find(Member.class, member.getId());
+            Team findTeam = findMember.getTeam();
 
-            System.out.println("member id : " + member1.getId());
-            System.out.println("member id : " + member2.getId());
-            System.out.println("member id : " + member3.getId());
-            System.out.println("=====================");
+            System.out.println("findTeam : " + findTeam.getName());
 
             tx.commit();
         }catch (Exception e){
--- ex1hellojpa/src/main/java/hellojpa/Team.java
+++ ex1hellojpa/src/main/java/hellojpa/Team.java
@@ -1,20 +1,19 @@
 package hellojpa;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 @Entity
-public class Member {
+public class Team {
 
     @Id
     @GeneratedValue
+    @Column(name = "TEAM_ID")
     private Long id;
 
-    @Column(name = "USERNAME")
     private String name;
-
-    @ManyToOne
-    @JoinColumn(name = "TEAM_ID")
-    private Team team;
 
     public Long getId() {
         return id;
@@ -30,13 +29,5 @@ public class Member {
 
     public void setName(String name) {
         this.name = name;
-    }
-
-    public Team getTeam() {
-        return team;
-    }
-
-    public void setTeam(Team team) {
-        this.team = team;
     }
 }
