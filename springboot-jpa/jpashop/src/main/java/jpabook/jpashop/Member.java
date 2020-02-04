d941bd345de3c4f399abb2871df4d7cbf835de05
Kim, Junha <kimjh4930@gmail.com>
2020-02-04 23:45:15 +0900

    JPA와 DB 설정, 동작확인

---
+++ springboot-jpa/jpashop/src/main/java/jpabook/jpashop/Member.java
@@ -0,0 +1,18 @@
+package jpabook.jpashop;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+public class Member {
+
+    @Id
+    private Long id;
+
+    private String username;
+}
